b00dfc820700311023e7be983f8d1d76d329c437
Rafael <munhra@munhra-VirtualBox.(none)>
2014-09-27 11:21:23 -0300

    teste

--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -8,6 +8,8 @@ using namespace cocos2d;
 class HelloWorld : public cocos2d::CCLayer
 {
 public:
+//teste
+//
     // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
     virtual bool init();  
 
